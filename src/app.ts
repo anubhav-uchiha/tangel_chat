4b5d982e7e67191765ab1e01ff46bfcf25f66d6c
Anubhav <anubhav27km@gmail.com>
2026-06-05 16:56:55 +0530

    code updated

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,8 @@ import express, { Application, Response } from "express";
 import cors from "cors";
 import authRoutes from "./routes/auth.route";
 import userRoutes from "./routes/user.route";
+import friendRequestRoutes from "./routes/friend-request.route";
+import messageRoutes from "./routes/message.route";
 import { errorMiddleware } from "./middlewares/error.middleware";
 
 const app: Application = express();
@@ -16,6 +18,8 @@ app.get("/", (_, res: Response) => {
 
 app.use("/api/auth", authRoutes);
 app.use("/api/user", userRoutes);
+app.use("/api/request", friendRequestRoutes);
+app.use("/api/message", messageRoutes);
 
 app.use(errorMiddleware);
 
